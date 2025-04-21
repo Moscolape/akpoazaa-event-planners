ba08e1fd7a6cc737288f9ce5a838135ba63d9af8
Moscolape <chunkums088@gmail.com>
2025-04-21 17:25:58 +0100

    chore: push first set of files

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center flex-col items-center">
+      <h1 className="text-red-700 text-3xl">Vite + React</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
